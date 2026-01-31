bcb16d91f6b2d281d3ac16f408815f27a4ed523e
BangZaky0029 <bangzaky0029@gmail.com>
2026-01-31 16:23:31 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-/// <reference types="vite/client" />
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: import.meta.env.VITE_APP_BASE_URL,
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    base: env.VITE_APP_BASE_URL || '/',
+    plugins: [react()],
+  }
 })
