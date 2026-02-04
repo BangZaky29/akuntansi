e83201ed804f4d53a463350ff62117fb22685c2d
BangZaky0029 <bangzaky0029@gmail.com>
2026-02-04 09:18:43 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-
-  return {
-    base: '/',
-    plugins: [react()],
-  }
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
 })
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/',
+// })
