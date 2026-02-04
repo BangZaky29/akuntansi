6b7bae3af6bd07a3378064ad5adc1c34ebd0dfeb
BangZaky0029 <bangzaky0029@gmail.com>
2026-02-04 09:14:00 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
-    base: env.VITE_APP_BASE_URL || '/',
+    base: '/',
     plugins: [react()],
   }
 })
